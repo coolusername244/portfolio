f413377223521ab8a1d7c8f3d116118abd1fbe1c
Lee <leesheppard2404@gmail.com>
2023-08-26 20:24:58 +0200

    fix(error with window object)

--- src/app/components/Navbar/NavItem.tsx
+++ src/app/components/Navbar/NavItem.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 
 type NavItemProps = {
@@ -7,7 +7,11 @@ type NavItemProps = {
 };
 
 const NavItem: React.FC<NavItemProps> = ({ item }) => {
-  const navbarHeight = 0.1 * window.innerHeight;
+  const [navbarHeight, setNavbarHeight] = React.useState(0);
+
+  useEffect(() => {
+    setNavbarHeight(0.1 * window.innerHeight);
+  }, []);
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
