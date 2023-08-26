c64ff146921c8aa896117a74a1b957c5c5cd168b
Lee <leesheppard2404@gmail.com>
2023-08-26 15:02:59 +0200

    feature(function to bring each page item to bottom of navar instead of going underneath)

--- src/app/components/Navbar/NavItem.tsx
+++ src/app/components/Navbar/NavItem.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
 
@@ -6,10 +7,29 @@ type NavItemProps = {
 };
 
 const NavItem: React.FC<NavItemProps> = ({ item }) => {
+  const navbarHeight = 0.1 * window.innerHeight;
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+
+    const target = document.querySelector(`#${item.toLowerCase()}`);
+    if (target) {
+      const offset = target.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: offset - navbarHeight,
+      });
+    }
+  };
+
   return (
-    <li className="m-4 cursor-pointer">
-      <Link href={`#${item.toLowerCase()}`}>{item}</Link>
-    </li>
+    <>
+      <li className="ml-5 cursor-pointer group hover:tracking-widest transition-all duration-2000">
+        <Link href={`#${item.toLowerCase()}`} onClick={handleClick}>
+          {item}
+        </Link>
+        <span className="block max-w-0 group-hover:max-w-full transition-all  h-[1px] bg-emerald-500"></span>
+      </li>
+    </>
   );
 };
 
