fde8b8f0abacde5625d4d1c2fd8aaf44b40bd62e
Lee <leesheppard2404@gmail.com>
2023-08-26 19:11:22 +0200

    refactor(updated import route for profile component)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile';
+import Profile from './components/Profile/Profile';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
