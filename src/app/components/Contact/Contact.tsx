daa4c42524ab44287cfb6f9b79a32a4f9b54dec3
Lee <leesheppard2404@gmail.com>
2023-08-29 20:46:57 +0200

    refactor(moved contact form into its own component folder and made the form its own component)

---
+++ src/app/components/Contact/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+import SocialIcons from './SocialIcons';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="h-[90vh] ">
+      <h2 className="text-4xl font-mono font-bold text-center pt-5">Contact</h2>
+      <ContactForm />
+      <ul className="flex w-[100%] md:w-[50vw] pt-10 md:pt-20 m-auto items-stretch justify-evenly md:justify-around text-4xl">
+        <SocialIcons />
+      </ul>
+    </section>
+  );
+};
+
+export default Contact;
