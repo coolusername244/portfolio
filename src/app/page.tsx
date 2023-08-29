6e302cb6c6c3086808f30dd5195ba595eba87a9e
Lee <leesheppard2404@gmail.com>
2023-08-29 20:44:26 +0200

    refactor(updated file imports)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer';
 
 export default function Home() {
