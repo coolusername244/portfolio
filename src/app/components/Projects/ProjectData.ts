b5efe53c7b16dead22328e013ac9c238a1e2ba40
Lee <leesheppard2404@gmail.com>
2023-08-30 21:40:03 +0200

    chore(added project data array)

---
+++ src/app/components/Projects/ProjectData.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    name: 'Stock Sense',
+    description:
+      'A stock trading simulation app designed for newcomers to practice and learn about stock trading risk free.',
+    githubLink: 'https://github.com/Lee-Lvan/stock-sense',
+    liveLink: 'https://stock-sense.netlify.app/',
+    techUsed: [
+      'TypeScript',
+      'NextJS 13',
+      'MongoDB',
+      'ApexChart',
+      'NextAuth',
+      'Material UI',
+      'API - Twelve Data',
+    ],
+  },
+  {
+    name: 'Match Up',
+    description:
+      'A card matching game where users will need to match all the cards in the deck before the time runs out!',
+    githubLink: 'https://github.com/coolusername244/match-up',
+    liveLink: 'https://coolusername244.github.io/match-up/',
+    techUsed: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+  },
+  {
+    name: 'Google Clone',
+    description:
+      "A front end clone of the well known Google Search Engine. Users are able to search for queries, images, do advanced queries and are also able to 'Feel Lucky'! When users make a search, they are forwarded to Google which displays their search results.",
+    githubLink: 'https://github.com/coolusername244/Google-frontend',
+    liveLink: 'https://coolusername244.github.io/Google-frontend/',
+    techUsed: ['HTML', 'CSS', 'Bootstrap'],
+  },
+];
